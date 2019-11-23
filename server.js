f77615644c6128dcef45e6008623b7a9cae55e8b
Ruben <34570952+thepixelboy@users.noreply.github.com>
2019-11-23 19:55:13 +0100

    The HTTP/HTTPS modules

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
-import config, {nodeEnv, logStars} from './config';
+import http from 'http';
 
-logStars('Function');
+const server = http.createServer();
+
+server.listen(8080);
+
+server.on('request', (req, res) => {
+    res.write('Hello HTTP!\n');
+    setTimeout(() => {
+        res.write('I can stream!\n');
+        res.end();
+    }, 3000);
+});
