f200e2162f99c4518ace4f1c603e767d1da47b79
Ruben <34570952+thepixelboy@users.noreply.github.com>
2019-11-24 17:35:59 +0100

    Using Sass with Node

--- src/components/ContestPreview.js
+++ src/components/ContestPreview.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const ContestPreview = (contest) => (
     <div className="ContestPreview">
-        <div>{contest.categoryName}</div>
-        <div>{contest.contestName}</div>
+        <div className="category-name">{contest.categoryName}</div>
+        <div className="contest-name">{contest.contestName}</div>
     </div>
 );
 
