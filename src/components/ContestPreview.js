2787a9e2f2d380847f6d5582a9c784c4613bccec
Ruben <34570952+thepixelboy@users.noreply.github.com>
2019-11-24 20:31:29 +0100

    Handling the contest click event

--- src/components/ContestPreview.js
+++ src/components/ContestPreview.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
-const ContestPreview = (contest) => (
-    <div className="ContestPreview">
-        <div className="category-name">{contest.categoryName}</div>
-        <div className="contest-name">{contest.contestName}</div>
-    </div>
-);
+class ContestPreview extends Component {
+    handleClick = () => {
+        console.log(this.props.contestName);
+    }
+    render() {
+        return (
+            <div className="link ContestPreview">
+                <div className="category-name" onClick={this.handleClick}>
+                    {this.props.categoryName}
+                </div>
+                <div className="contest-name">
+                    {this.props.contestName}
+                </div>
+            </div>
+        );
+    }
+}
+
+ContestPreview.propTypes = {
+    categoryName: PropTypes.string.isRequired,
+    contestName: PropTypes.string.isRequired,
+}
 
 export default ContestPreview;
