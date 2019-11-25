7f57ad4c92f0fea03a2fab0fd2a4e4d1f829d861
Ruben <34570952+thepixelboy@users.noreply.github.com>
2019-11-25 10:23:06 +0100

    Navigating to a contest

--- src/components/ContestList.js
+++ src/components/ContestList.js
@@ -3,16 +3,20 @@ import PropTypes from 'prop-types';
 
 import ContestPreview from './ContestPreview';
 
-const ContestList = ({contests}) => (
+const ContestList = ({contests, onContestClick}) => (
     <div className="ContestList">
         {contests.map(contest =>
-            <ContestPreview key={contest.id} {...contest} />
+            <ContestPreview 
+                key={contest.id} 
+                onClick={onContestClick}
+                {...contest} />
         )}                    
     </div>
 );
 
 ContestList.propTypes = {
-    contests: PropTypes.array
+    contests: PropTypes.array,
+    onContestClick: PropTypes.func.isRequired,
 };
 
 export default ContestList;
