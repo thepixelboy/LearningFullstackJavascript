3f16e031d0f67ab24bc3ae0347a047bb10992df3
Ruben <34570952+thepixelboy@users.noreply.github.com>
2019-11-26 15:21:41 +0100

    API to fetch a list of names

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ export const logStars = function(message) {
     console.info('**********');
 };
 
-export default {
-    'mongodb+srv://yourUser:yourPassword@actinium-dvo5d.mongodb.net/test?retryWrites=true&w=majority',    
+export default {    
+    mongodbUri: '',
     port: env.PORT || 8080,
     host: env.HOST || '0.0.0.0',
     get serverUrl() {
